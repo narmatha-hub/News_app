61a38eb265f98781d7ef9e5e06a79d4db45e8a55
narmatha-hub <narmathaelangovan610@gmail.com>
2025-03-08 12:23:28 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,26 @@
+// import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import Home from './pages/Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import CategoryPage from './pages/CategoryPage';
+import NewsPage from './pages/NewsPage';
+import NavbarComponent from './components/NavbarComponent';
+import Footer from './components/Footer';
+
+function App() {
+
+  return (
+    <div className="App">
+        <NavbarComponent />
+        <Routes>
+          <Route exact path="/" element={<Home/>} />
+          <Route exact path="/category/:id" element={<CategoryPage/>} />
+          <Route exact path="/news/:id" element={<NewsPage/>} />
+        </Routes>
+        <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/context/GeneralContext.jsx
@@ -0,0 +1,59 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+export const GeneralContext = React.createContext();
+const GeneralContextProvider = ({children}) => {
+
+    const [topNews, setTopNews] = useState([])
+
+    const [businessNews, setBusinessNews] = useState([]);
+    const [technologyNews, setTechnologyNews] = useState([]);
+    const [politicsNews, setPoliticsNews] = useState([]);
+
+    useEffect(() => { 
+        fetchTopNews() 
+        fetchBusinessNews()
+        fetchPoliticsNews()
+        fetchTechnologyNews()
+      }, [])
+    
+      const fetchTopNews = async () => {
+        try {
+          const response = await axios.get("https://newsapi.org/v2/everything?q=popular&apiKey=1265ae7ddae84495943a5943c3d4fcdd");
+          setTopNews(response.data.articles);
+        } catch (error) {
+          console.error(error);
+        }
+      }
+
+      const fetchBusinessNews = async () => {
+        try {
+          const response = await axios.get("https://newsapi.org/v2/everything?q=business&apiKey=1265ae7ddae84495943a5943c3d4fcdd");
+          setBusinessNews(response.data.articles);
+        } catch (error) {
+          console.error(error);
+        }
+      }
+      const fetchPoliticsNews = async () => {
+        try {
+          const response = await axios.get("https://newsapi.org/v2/everything?q=politics&apiKey=1265ae7ddae84495943a5943c3d4fcdd");
+          setPoliticsNews(response.data.articles);
+        } catch (error) {
+          console.error(error);
+        }
+      }
+      const fetchTechnologyNews = async () => {
+        try {
+          const response = await axios.get("https://newsapi.org/v2/everything?q=technology&apiKey=1265ae7ddae84495943a5943c3d4fcdd");
+          setTechnologyNews(response.data.articles);
+        } catch (error) {
+          console.error(error);
+        }
+      }
+
+    
+  return (
+    <GeneralContext.Provider value={{topNews, businessNews, technologyNews, politicsNews}} >{children}</GeneralContext.Provider>
+  )
+}
+
+export default GeneralContextProvider
